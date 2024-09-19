b46055992a47abf26da8ab038a8e917d7d33e5ff
AxelF12 <157697299+AxelF12@users.noreply.github.com>
2024-09-19 08:20:21 +0200

    Update state_machine.cpp

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -12,37 +12,36 @@ void state_machine(Event_t event)
     if (INPUT_1_ACCEPTED == event)
     {
       state = LEVEL_1_UNLOCKED;
-      motor_geschlossen (); //neu
+
     }
     break;
   case LEVEL_1_UNLOCKED:
     if (INPUT_2_ACCEPTED == event)
     {
       state = LEVEL_2_UNLOCKED;
-      motor_1_geoffnet (); //neu
+
     }
     else if (INPUT_REFUSED == event)
     {
       state = SAFE_LOCKED;
-      motor_geschlossen (); //neu
+   
     }
     break;
   case LEVEL_2_UNLOCKED:
     if (INPUT_3_ACCEPTED == event)
     {
       state = LEVEL_3_UNLOCKED;
-      motor_2_geoffnet (); //neu
+    
     }
     else if (INPUT_REFUSED == event)
     {
       state = SAFE_LOCKED;
-      motor_geschlossen (); //neu
+   
     }
     break;
   case LEVEL_3_UNLOCKED:
     if (OPEN_DOOR == event)
-    { motor_1_geoffnet (); //neu
-      motor_2_geoffnet (); //neu
+    { 
       state = SAFE_OPEN;
     }
     break;
@@ -50,7 +49,7 @@ void state_machine(Event_t event)
     if (CLOSE_DOOR == event)
     {
       state = SAFE_LOCKED;
-      motor_geschlossen (); //neu
+
     }
     break;
   }
